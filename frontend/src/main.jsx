147b77bc892461113505d4373ddca9f5b4621545
Vishal Barde <bardevishal92@gmail.com>
2026-09-21 15:19:19 +0530

    navbar changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { WishlistProvider } from './context/WishlistContext.jsx'
+import Navbar from './components/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <WishlistProvider>
+      
         <App />
       </WishlistProvider>
     </BrowserRouter>
